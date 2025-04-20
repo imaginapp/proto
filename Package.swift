9bccd554c89cc2206b5239fb9dbe5db165845a0b
Nicholas Terrell <nicholas@terrell.com.au>
2025-04-20 16:02:56 +1000

    swift update

--- Package.swift
+++ Package.swift
@@ -13,24 +13,17 @@ let package = Package(
             targets: ["ImaginProto"])
     ],
     dependencies: [
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
-        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
-        .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
         .target(
             name: "ImaginProto",
             dependencies: [
-                .product(name: "GRPCCore", package: "grpc-swift"),
-                .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
-                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+                .product(name: "GRPC", package: "grpc-swift"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "swift/Sources/Generated",
-            plugins: [
-                .plugin(name: "GRPCProtobufGenerator", package: "grpc-swift-protobuf")
-            ]
         )
     ]
 )
