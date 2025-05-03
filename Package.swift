51aa0e85d0ad62071a5675d18e9251d26440d1ab
Nicholas Terrell <nicholas@terrell.com.au>
2025-05-03 16:47:46 +1000

    update swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ImaginProto",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v15),
+        .iOS(.v13)
     ],
     products: [
         .library(
